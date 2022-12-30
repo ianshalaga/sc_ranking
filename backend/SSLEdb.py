c5c5854c46bf26835e26064ae1768d57a4c415bd
Darién Julián Ramírez <ianshalaga@gmail.com>
2022-12-30 15:42:06 -0300

    Update SSLEdb.py

--- backend/SSLEdb.py
+++ backend/SSLEdb.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import declarative_base, relationship, Session
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, or_, ForeignKey
+from enum import Enum
 
 
 ''' Engine '''
@@ -9,6 +10,16 @@ DB_API = "pysqlite"
 DB_PATH = "LÑdb.db"
 ENGINE = create_engine(f"{DB_ENGINE}+{DB_API}:///{DB_PATH}", echo=True, future=True)
 # STATUS_CODES_ERROR = [404, 500]
+
+class RoundResult(Enum):
+    D = 240
+    PW = 240
+    W = 240
+    WY = 168
+    WB = 84
+    LY = 168
+    LB = 84
+    PL = 0
 
 
 ''' ClassTables '''
@@ -164,6 +175,7 @@ class Duel(Base):
 
     duel_type = relationship("DuelType", back_populates="duels")
     event = relationship("Event", back_populates="duels")
+    combats = relationship("Combat", back_populates="duel", cascade="all, delete-orphan")
 
 
 class DuelType(Base):
@@ -186,8 +198,19 @@ class Combat(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     combat_order = Column(Integer, nullable=False)
     stage_id = Column(Integer, ForeignKey("stage.id"), nullable=False)
+    duel_id = Column(Integer, ForeignKey("duel.id"), nullable=False)
+    player1_id = Column(Integer, ForeignKey("player.id"), nullable=False)
+    player2_id = Column(Integer, ForeignKey("player.id"), nullable=False)
+    character1_id = Column(Integer, ForeignKey("character.id"), nullable=False)
+    character2_id = Column(Integer, ForeignKey("character.id"), nullable=False)
 
     stage = relationship("Stage", back_populates="combats")
+    duel = relationship("Duel", back_populates="combats")
+    player1 = relationship("Player", back_populates="combats_p1")
+    player2 = relationship("Player", back_populates="combats_p2")
+    character1 = relationship("Character", back_populates="combats_p1")
+    character2 = relationship("Character", back_populates="combats_p2")
+    rounds = relationship("Round", back_populates="combat", cascade="all, delete-orphan")
 
     ''' Methods '''
     def get_rounds_played():
@@ -214,3 +237,138 @@ class Round(Base):
     ''' Attributes '''
     id = Column(Integer, primary_key=True, nullable=False)
     round_order = Column(Integer, nullable=False)
+    result_player1 = Column(String, nullable=False)
+    result_player2 = Column(String, nullable=False)
+    combat_id = Column(Integer, ForeignKey("combat.id"), nullable=False)
+
+    combat = relationship("Combat", back_populates="rounds")
+
+
+class Player(Base):
+    __tablename__ = "player"
+
+    ''' Attributes '''
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    playlist = Column(String, nullable=True)
+
+    combats_p1 = relationship("Combat", back_populates="player1", cascade="all, delete-orphan")
+    combats_p2 = relationship("Combat", back_populates="player2", cascade="all, delete-orphan")
+
+    ''' Methods '''
+    def get_events_played_list():
+        return
+
+    def get_event_position():
+        return
+
+    def get_events_played_number():
+        return
+
+    def get_duels_played_list():
+        return
+
+    def get_duels_played_number():
+        return
+
+    def get_duels_won_list():
+        return
+
+    def get_duels_won_number():
+        return
+
+    def get_duels_lost_list():
+        return
+
+    def get_duels_lost_number():
+        return
+
+    def get_team_duels_played_list():
+        return
+
+    def get_team_duels_played_number():
+        return
+
+    def get_team_duels_won_list():
+        return
+
+    def get_team_duels_won_number():
+        return
+
+    def get_team_duels_lost_list():
+        return
+
+    def get_team_duels_lost_number():
+        return
+
+    def get_combats_played_list():
+        return
+
+    def get_combats_played_number():
+        return
+
+    def get_combats_won_list():
+        return
+
+    def get_combats_won_number():
+        return
+
+    def get_combats_lost_list():
+        return
+
+    def get_combats_lost_number():
+        return
+
+    def get_combats_draw_list():
+        return
+
+    def get_combats_draw_number():
+        return
+
+    def get_rounds_played_number():
+        return
+
+    def get_rounds_won_number():
+        return
+
+    def get_rounds_lost_number():
+        return
+
+    def get_duels_win_rate():
+        return
+
+    def get_team_duels_win_rate():
+        return
+
+    def get_combats_win_rate():
+        return
+
+    def get_rounds_win_rate():
+        return
+
+    def get_duels_wlr():
+        return
+
+    def get_team_duels_wlr():
+        return
+
+    def get_combats_wlr():
+        return
+
+    def get_rounds_wlr():
+        return
+
+    def get_player_level():
+        return
+
+
+class Character(Base):
+    __tablename__ = "character"
+
+    ''' Attributes '''
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    playlist = Column(String, nullable=True)
+
+    combats_p1 = relationship("Combat", back_populates="character1", cascade="all, delete-orphan")
+    combats_p2 = relationship("Combat", back_populates="character2", cascade="all, delete-orphan")
